b3fc3f0a6635d14e4e182aef42c275eaf9820db0
Pierre Mdawar <pierre@mdawar.dev>
2022-06-25 10:01:08 +0300

    [tests] Added factory functions to be used in the tests

--- tests/memoize.test.ts
+++ tests/memoize.test.ts
@@ -1,9 +1,82 @@
 import { memoize } from '../src/index.js';
 
+/** Sync function factory. */
+function makeFn() {
+  let throwNext = false;
+
+  return function (...agrs: any[]) {
+    throwNext = !throwNext;
+
+    if (throwNext) {
+      throw new Error('Failed');
+    }
+
+    return true;
+  };
+}
+
+/** Async function factory. */
+function makeAsyncFn() {
+  let throwNext = false;
+
+  return async function (...agrs: any[]) {
+    throwNext = !throwNext;
+
+    if (throwNext) {
+      throw new Error('Failed');
+    }
+
+    return true;
+  };
+}
+
+/** Sync counter function factory. */
+function makeCounter() {
+  let count = 0;
+  return (...agrs: any[]) => count++;
+}
+
+/** Async counter function factory. */
+function makeAsyncCounter() {
+  let count = 0;
+  return async (...agrs: any[]) => count++;
+}
+
+describe('Factory functions', () => {
+  test('Synchronous function', () => {
+    const fn = makeFn();
+
+    expect(() => fn()).toThrow('Failed');
+    expect(fn()).toBe(true);
+  });
+
+  test('Async function', async () => {
+    const asyncFn = makeAsyncFn();
+
+    await expect(asyncFn()).rejects.toThrow('Failed');
+    await expect(asyncFn()).resolves.toBe(true);
+  });
+
+  test('Synchronous counter function', () => {
+    const counter = makeCounter();
+
+    expect(counter()).toBe(0);
+    expect(counter()).toBe(1);
+    expect(counter()).toBe(2);
+  });
+
+  test('Async counter function', async () => {
+    const counter = makeAsyncCounter();
+
+    expect(await counter()).toBe(0);
+    expect(await counter()).toBe(1);
+    expect(await counter()).toBe(2);
+  });
+});
+
 describe('Basic tests', () => {
   test('Memoized function result is cached', () => {
-    let index = 0;
-    const counter = () => index++;
+    const counter = makeCounter();
 
     const memoized = memoize(counter);
 
